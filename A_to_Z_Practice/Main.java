77998ba9245baecbdd9376dae9955a059e740a57
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2025-12-17 23:25:49 +0530

    add inheritance example
     inheritance.java, demonstrating basic inheritance with Animal and Dog classes.

--- A_to_Z_Practice/Main.java
+++ A_to_Z_Practice/Main.java
@@ -1,23 +1,21 @@
-// Parent class (Super class)
+
 class Animal {
     void eat() {
         System.out.println("This animal eats food");
     }
 }
 
-// Child class (Sub class)
 class Dog extends Animal {
     void bark() {
         System.out.println("The dog barks");
     }
 }
 
-// Main class
-public class inheritance {
+public class Main {
     public static void main(String[] args) {
         Dog d = new Dog();
 
-        d.eat();   // inherited method
-        d.bark();  // child class method
+        d.eat();
+        d.bark();
     }
 }
