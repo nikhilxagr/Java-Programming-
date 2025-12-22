339c28ce4afdca841436a304eaaf75ab8f8e1f4d
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2025-12-22 23:32:14 +0530

    Add inheritance example a
    Added InheritanceExample.java to demonstrate basic inheritance and method usage in Java.

--- A_to_Z_Practice/Class Object Methods/MethodExample.java
+++ A_to_Z_Practice/Class Object Methods/MethodExample.java
@@ -3,7 +3,7 @@ class Student {
     String name;
     int age;
 
-    // Method to display student details
+    
     void displayInfo() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
@@ -17,11 +17,11 @@ public class MethodExample {
         Student s1;
         s1 = new Student();
 
-        // Assign values using object reference
+       
         s1.name = "Rahul";
         s1.age = 20;
 
-        // Method call using object reference
+      
         s1.displayInfo();
     }
 }
